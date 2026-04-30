855fbaa88cfa0b62cd1d7e63eebaf54d6ba56caf
小野弘明 <hiro2804@icloud.com>
2026-04-30 17:01:03 +0900

    20260430 yoyogi

--- api/serializers.py
+++ api/serializers.py
@@ -8,7 +8,7 @@ from app.models import Task, Estimate, Unit, Reference
 class UnitSerializer(serializers.ModelSerializer):
     # id = serializers.CharField(required=False, allow_blank=True)
     # unit_no = serializers.CharField(required=True)
-    # unit_name = serializers.CharField(required=True)
+    unit_name = serializers.CharField(required=True)
 
     class Meta:
         model = Unit
@@ -23,30 +23,10 @@ class UnitSerializer(serializers.ModelSerializer):
             'unit_name': {
                 'error_messages': {
                     'required': "必須の入力項目です。5555",
-                    'blank': "空欄にはできません。",
+                    # 'blank': "空欄にはできません。",
                 }
             }
         }
-
-    # def create(self, validated_data):
-    #     # IDがある場合は更新、ない場合は作成
-    #     request = self.context.get('request')
-    #     raw_id = request.data.get('id') if request else None
-    #
-    #     # 数値（または数値文字列）の場合のみ既存データを探す
-    #     if raw_id is not None:
-    #         # 文字列 '123' も数値 123 も判定できる方法
-    #         str_id = str(raw_id)
-    #         if str_id.isdigit():
-    #             instance = Unit.objects.filter(id=int(str_id)).first()
-    #             if instance:
-    #                 # 既存データがあれば更新
-    #                 return self.update(instance, validated_data)
-    #
-    #     # IDが「数値ではない文字列（u78787等）」または「存在しないID」の場合は新規作成
-    #     # validated_dataからidを除去して、自動採番(AutoField)を確実に動かす
-    #     validated_data.pop('id', None)
-    #     return super().create(validated_data)
 
 
 class ReferenceSerializer(serializers.ModelSerializer):
--- unit/views.py
+++ unit/views.py
@@ -19,244 +19,184 @@ import json
 from django.http import JsonResponse
 
 
-def unit_list(request):
+def unit_list_call(request):
     return render(request, 'unit_list.html')
 
 
-@api_view(['POST'])
-def save_unit_api(request):
-    if request.method == 'POST':
-        data = request.data
-
-        raw_id = data.get('id')
-        instance = None
-        if isinstance(raw_id, (int, str)) and str(raw_id).isdigit():
-            instance = Unit.objects.filter(id=raw_id).first()
-
-        if instance:
-            # 更新 (Partial=True で一部フィールドのみの更新にも対応)
-            serializer = UnitSerializer(instance, data=data, partial=True)
-        else:
-            # 新規作成 (一時IDなどはシリアライザに渡さないように id を除外)
-            item_data = data.copy()
-            item_data.pop('id', None)
-            serializer = UnitSerializer(data=item_data)
-
-        if serializer.is_valid():
-            saved_instance = serializer.save()
-            return Response({
-                'success': True,
-                'id': saved_instance.id,
-                'data': UnitSerializer(saved_instance).data
-            }, status=status.HTTP_200_OK)
-        else:
-            # バリデーションエラー時は例外を投げてロールバックさせる
-            return Response({
-                "error": "validation Failed",
-                "details": serializer.errors,
-                "item": data
-            }, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
-
-        # 更新の場合は instance を渡す（IDなどで判断）
-        # instance = Unit.objects.filter(id=data.get('id')).first()
-        # form = UnitForm(data, instance=instance)
-        #
-        # if form.is_valid():
-        #     obj = form.save()
-        #     return JsonResponse({
-        #         'success': True,
-        #         'id': obj.id})  # 新しく振られたIDを返す
-        # else:
-        #     # unique=True などのエラーメッセージを取得
-        #     msg = " / ".join([e[0] for e in form.errors.values()])
-        #     return JsonResponse({'success': False, 'message': msg})
-
-
-# @api_view(['POST'])
-# def batch_save(request):
-#     # request.data自体がリスト（配列）なので、直接ループを回す
-#
-#     data_list = request.data
-#     if not isinstance(data_list, list):
-#         return Response({"error": "データがリスト形式ではありません"}, status=status.HTTP_400_BAD_REQUEST)
-#     results = []
-#     try:
-#         with transaction.atomic():
-#             # 送られてきたリストをループ処理
-#             for item in request.data:
-#                 raw_id = item.get('id')
-#
-#                 # 既存データの更新か新規作成かを判定
-#                 # 数値型であり、かつ実際にDBに存在するIDか確認
-#                 instance = None
-#                 if isinstance(raw_id, (int, str)) and str(raw_id).isdigit():
-#                     instance = Unit.objects.filter(id=raw_id).first()
-#
-#                 if instance:
-#                     # 更新 (Partial=True で一部フィールドのみの更新にも対応)
-#                     serializer = UnitSerializer(instance, data=item, partial=True)
-#                 else:
-#                     # 新規作成 (一時IDなどはシリアライザに渡さないように id を除外)
-#                     item_data = item.copy()
-#                     item_data.pop('id', None)
-#                     serializer = UnitSerializer(data=item_data)
-#
-#                 if serializer.is_valid():
-#                     saved_instance = serializer.save()
-#                     results.append(UnitSerializer(saved_instance).data)
-#                 else:
-#                     # バリデーションエラー時は例外を投げてロールバックさせる
-#                     return Response({
-#                         "error": "validation Failed",
-#                         "details": serializer.errors,
-#                         "item": item
-#                     }, status=status.HTTP_400_BAD_REQUEST)
-#
-#             # 全ての処理が成功した場合のみここに来る
-#             return Response(results, status=status.HTTP_200_OK)
-#     except Exception as e:
-#         # 処理後の全データをリストで返却
-#         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-# class UnitViewSet(viewsets.ModelViewSet):
-#     queryset = Unit.objects.all()
-#     serializer_class = UnitSerializer
-
-
-@api_view(['GET'])
-def data_list(request, offset):
+@api_view(['GET', 'POST'])
+def unit_list(request):
     if request.method == 'GET':
-        units = Unit.objects.order_by('unit_no').all()
-        # tasks = Task.objects.all()
-        unitData = UnitSerializer(units, many=True)
-        # taskData = TaskSerializer(tasks, many=True)
-        return Response({
-            "units": unitData.data,
-            # "tasks": taskData.data
-        })
-
-
-@api_view(['POST'])
-def unit_add(request):
-    if request.method == 'POST':
-        serializer = UnitSerializer(
-            data=request.data,
-            many=isinstance(request.data, list),
-            context={'request': request})
-        print(serializer)
-
+        units = Unit.objects.all()
+        serializer = UnitSerializer(units, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = UnitSerializer(data=request.data)
         if serializer.is_valid():
-            units = serializer.save()
-            # DHTMLXのグリッド更新用に、保存された全データをリストで返す
-            return Response(
-                UnitSerializer(units, many=True, context={'request': request}).data, status=status.HTTP_201_CREATED)
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "PATCH", "DELETE"])
+def unit_detail(request, pk):
+    try:
+        instance = Unit.objects.get(pk=pk)
+    except Unit.DoesNotExist:
+        return Response({"detail": "対象が見つかりません"}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "DELETE":
+        try:
+            instance.delete()
+            return JsonResponse({'success': True}, status=status.HTTP_200_OK)
+        except models.ProtectedError as e:
+            msg = f'「{instance}」は他で使われているため削除がきません'
+            return Response({"detail": msg}, status=status.HTTP_400_BAD_REQUEST)
+
+    # 3. 取得処理 (GET)
+    if request.method == "GET":
+        serializer = UnitSerializer(instance)
+        return Response(serializer.data)
+
+    serializer = UnitSerializer(instance, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        # DHTMLX側が期待する「更新後のデータ単体」を返す
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        # バリデーションエラー時はシリアライザのエラーをそのまま返す
+        # これによりJSの .catch(err => { ... }) で項目ごとにエラー表示が可能
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@require_http_methods(["DELETE", "POST"])
-def delete_unit(request, pk):
-    try:
-        obj = Unit.objects.get(pk=pk)
-        obj.delete()
-        return JsonResponse({'success': True})
-    except Unit.DoesNotExist:
-        return JsonResponse({'success': False, 'message': '対象が見つかりません'}, status=404)
-    except models.ProtectedError as e:
-        return JsonResponse({'success': False, 'message': f'「{obj}」他で使われているため削除がきません'}, status=400)
-
-
+#
+# @api_view(['GET'])
+# def data_list(request, offset):
+#     if request.method == 'GET':
+#         units = Unit.objects.order_by('unit_no').all()
+#         # tasks = Task.objects.all()
+#         unitData = UnitSerializer(units, many=True)
+#         # taskData = TaskSerializer(tasks, many=True)
+#         return Response({
+#             "units": unitData.data,
+#             # "tasks": taskData.data
+#         })
+#
+#
 # @api_view(['POST'])
-# def task_add(request):
+# def unit_add(request):
 #     if request.method == 'POST':
-#         serializer = TaskSerializer(data=request.data)
+#         serializer = UnitSerializer(
+#             data=request.data,
+#             many=isinstance(request.data, list),
+#             context={'request': request})
 #         print(serializer)
 #
 #         if serializer.is_valid():
-#             task = serializer.save()
-#             return JsonResponse({'action': 'inserted', 'tid': task.id})
-#         return JsonResponse({'action': 'error'})
-
-
-# @api_view(['PUT', 'DELETE'])
-# def task_update(request, pk):
+#             units = serializer.save()
+#             # DHTMLXのグリッド更新用に、保存された全データをリストで返す
+#             return Response(
+#                 UnitSerializer(units, many=True, context={'request': request}).data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# @require_http_methods(["DELETE", "POST"])
+# def delete_unit(request, pk):
 #     try:
-#         task = Task.objects.get(pk=pk)
-#     except Task.DoesNotExist:
-#         return JsonResponse({'action': 'error2'})
+#         obj = Unit.objects.get(pk=pk)
+#         obj.delete()
+#         return JsonResponse({'success': True})
+#     except Unit.DoesNotExist:
+#         return JsonResponse({'success': False, 'message': '対象が見つかりません'}, status=404)
+#     except models.ProtectedError as e:
+#         return JsonResponse({'success': False, 'message': f'「{obj}」他で使われているため削除がきません'}, status=400)
 #
-#     if request.method == 'PUT':
-#         serializer = TaskSerializer(task, data=request.data)
-#         print(serializer)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse({'action': 'updated'})
-#         return JsonResponse({'action': 'error'})
 #
-#     if request.method == 'DELETE':
-#         task.delete()
-#         return JsonResponse({'action': 'deleted'})
-
-
-class UnitList(generic.ListView):
-    paginate_by = 10
-    context_object_name = 'unit_list'
-    template_name = 'unit_list.html'
-    model = Unit
-    form_class = UnitForm
-
-    def get_queryset(self):
-        return Unit.objects.order_by('unit_no').all()
-
-
-class UnitAdd(generic.FormView):
-    model = Unit
-    template_name = 'unit_add.html'
-    form_class = UnitForm
-    success_url = reverse_lazy('unit:unit_list')
-
-    def form_valid(self, form):
-        form.save()
-        messages.success(self.request, '単位を追加しました。')
-        return super().form_valid(form)
-
-    def form_invalid(self, form):
-        messages.error(self.request, "単位の追加に失敗しました。")
-        return super().form_invalid(form)
-
-
-class UnitEdit(generic.UpdateView):
-    model = Unit
-    template_name = 'unit_edit.html'
-    form_class = UnitForm
-
-    def get_success_url(self):
-        return reverse_lazy('unit:unit_list')
-
-    def form_valid(self, form):
-        messages.success(self.request, '単位を更新しました。')
-        return super().form_valid(form)
-
-    def form_invalid(self, form):
-        messages.error(self.request, "単位の更新に失敗しました。")
-        return super().form_invalid(form)
-
-
-class UnitDel(generic.DeleteView):
-    model = Unit
-    template_name = 'unit_del.html'
-
-    def post(self, request, *args, **kwargs):
-
-        try:
-            obj = self.get_object()
-            obj.delete()
-            messages.success(self.request, "単位を削除しました。")
-            return redirect('unit:unit_list')
-        except models.ProtectedError as e:
-            messages.error(request, f'「{obj}」は他で使われているため削除がきません。')
-            return redirect('unit:unit_list')
+# # @api_view(['POST'])
+# # def task_add(request):
+# #     if request.method == 'POST':
+# #         serializer = TaskSerializer(data=request.data)
+# #         print(serializer)
+# #
+# #         if serializer.is_valid():
+# #             task = serializer.save()
+# #             return JsonResponse({'action': 'inserted', 'tid': task.id})
+# #         return JsonResponse({'action': 'error'})
+#
+#
+# # @api_view(['PUT', 'DELETE'])
+# # def task_update(request, pk):
+# #     try:
+# #         task = Task.objects.get(pk=pk)
+# #     except Task.DoesNotExist:
+# #         return JsonResponse({'action': 'error2'})
+# #
+# #     if request.method == 'PUT':
+# #         serializer = TaskSerializer(task, data=request.data)
+# #         print(serializer)
+# #         if serializer.is_valid():
+# #             serializer.save()
+# #             return JsonResponse({'action': 'updated'})
+# #         return JsonResponse({'action': 'error'})
+# #
+# #     if request.method == 'DELETE':
+# #         task.delete()
+# #         return JsonResponse({'action': 'deleted'})
+#
+#
+# class UnitList(generic.ListView):
+#     paginate_by = 10
+#     context_object_name = 'unit_list'
+#     template_name = 'unit_list.html'
+#     model = Unit
+#     form_class = UnitForm
+#
+#     def get_queryset(self):
+#         return Unit.objects.order_by('unit_no').all()
+#
+#
+# class UnitAdd(generic.FormView):
+#     model = Unit
+#     template_name = 'unit_add.html'
+#     form_class = UnitForm
+#     success_url = reverse_lazy('unit:unit_list')
+#
+#     def form_valid(self, form):
+#         form.save()
+#         messages.success(self.request, '単位を追加しました。')
+#         return super().form_valid(form)
+#
+#     def form_invalid(self, form):
+#         messages.error(self.request, "単位の追加に失敗しました。")
+#         return super().form_invalid(form)
+#
+#
+# class UnitEdit(generic.UpdateView):
+#     model = Unit
+#     template_name = 'unit_edit.html'
+#     form_class = UnitForm
+#
+#     def get_success_url(self):
+#         return reverse_lazy('unit:unit_list')
+#
+#     def form_valid(self, form):
+#         messages.success(self.request, '単位を更新しました。')
+#         return super().form_valid(form)
+#
+#     def form_invalid(self, form):
+#         messages.error(self.request, "単位の更新に失敗しました。")
+#         return super().form_invalid(form)
+#
+#
+# class UnitDel(generic.DeleteView):
+#     model = Unit
+#     template_name = 'unit_del.html'
+#
+#     def post(self, request, *args, **kwargs):
+#
+#         try:
+#             obj = self.get_object()
+#             obj.delete()
+#             messages.success(self.request, "単位を削除しました。")
+#             return redirect('unit:unit_list')
+#         except models.ProtectedError as e:
+#             messages.error(request, f'「{obj}」は他で使われているため削除がきません。')
+#             return redirect('unit:unit_list')
